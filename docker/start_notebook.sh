92981ae7e7f0ac71ec53aa8066b48e3080124950
root <root@localhost.localdomain>
2022-08-01 03:38:19 -0300

    Initial commit

--- docker/start_notebook.sh
+++ docker/start_notebook.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/bash env 
-docker container stop $(docker container ls -aq)
-docker container rm $(docker container ls -aq)
-docker system prune -a
-
-
-
+sudo docker run -it --rm -p 8889:8888 -v /home/rhel9dev/data_engineer_test:/home/jovyan/work jupyter/all-spark-notebook

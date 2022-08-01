40c0bd1dea7710d00aa2ed9dbc6b667a5ab93991
root <root@localhost.localdomain>
2022-08-01 02:33:41 -0300

    Initial commit

--- src/create_cronjob_etl.sh
+++ src/create_cronjob_etl.sh
@@ -1,4 +1,4 @@
 !/usr/bin/bash env 
-(crontab -l 2>/dev/null; echo "*/5 * * * * jupyter nbconvert --execute /home/jovyan/work/etl.ipynb") | crontab -
+(crontab -l 2>/dev/null; echo "0 0 1 * * jupyter nbconvert --execute /home/jovyan/work/etl.ipynb") | crontab -
 
 

e0a2e87c696f83da4a4355699056e0118effe2b8
aspirina765 <48143182+aspirina765@users.noreply.github.com>
2022-08-01 04:16:08 -0300

    Update start_notebook.sh

--- docker/start_notebook.sh
+++ docker/start_notebook.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash env 
-docker run -it --rm -p 8889:8888 -v /home/rhel9dev/data_engineer_test:/home/jovyan/work jupyter/all-spark-notebook
+docker run -it --rm -p 8889:8888 -v /path/to/project:/home/jovyan/work jupyter/all-spark-notebook
